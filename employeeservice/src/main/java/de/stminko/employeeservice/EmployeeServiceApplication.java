5c4de062068ef9d369c56b85d8685a0e2557919e
Stéphan Minko <stephan.minko@nexnet.de>
2023-12-10 03:25:05 +0100

    add some optimization

--- employeeservice/src/main/java/de/stminko/employeeservice/EmployeeServiceApplication.java
+++ employeeservice/src/main/java/de/stminko/employeeservice/EmployeeServiceApplication.java
@@ -4,7 +4,10 @@ import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.auditing.DateTimeProvider;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
@@ -17,7 +20,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  * {@code @EnableJpaRepositories}, which enable JPA auditing and configure the repository
  * layer, respectively. The {@code @EnableJpaRepositories} annotation is specifically
  * configured to use {@code EnversRevisionRepositoryFactoryBean} for integration with
- * Hibernate Envers, facilitating auditing of the JPA entities.
+ * Hibernate Envers, facilitating auditing of the JPA entities as well as enabling Jpa
+ * Auditing via annotation, log the referenced {@link AuditorAware} and
+ * {@link DateTimeProvider} instance information of the beans used.
  * </p>
  *
  * <p>
@@ -29,6 +34,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  * @author Stéphan Minko
  */
 @EnableJpaRepositories(repositoryFactoryBeanClass = EnversRevisionRepositoryFactoryBean.class)
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider", dateTimeProviderRef = "dateTimeProvider")
 @SpringBootApplication
 public class EmployeeServiceApplication {
 
--- employeeservice/src/main/java/de/stminko/employeeservice/runtime/persistence/boundary/JpaConfiguration.java
+++ employeeservice/src/main/java/de/stminko/employeeservice/runtime/persistence/boundary/JpaConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  * configure the CustomAuditorAware bean.
@@ -28,24 +27,6 @@ public class JpaConfiguration {
 	@Bean
 	public DateTimeProvider dateTimeProvider() {
 		return () -> Optional.of(ZonedDateTime.now());
-	}
-
-	/**
-	 * enable Jpa Auditing via annotation, log the referenced {@link AuditorAware} and
-	 * {@link DateTimeProvider} instance information of the beans used.
-	 *
-	 * @author Thomas Zirke
-	 */
-	@EnableJpaAuditing(auditorAwareRef = "auditorProvider", dateTimeProviderRef = "dateTimeProvider")
-	static class SpringEnversAuditingConfiguration {
-
-		SpringEnversAuditingConfiguration(AuditorAware<?> auditorAware, DateTimeProvider dateTimeProvider) {
-			log.info(
-					"SpringEnversAuditionConfiguration applied, JPA Auditing enabled, using AuditorAware bean of Type:"
-							+ " [{}], using DateTimeProvider of Type: [{}]",
-					auditorAware.getClass().getName(), dateTimeProvider.getClass().getName());
-		}
-
 	}
 
 }
